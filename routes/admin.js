85ab5a23c5e3a470d1ef62ed1b717b91e2d9c68e
tbisho <tess.bishop8@gmail.com>
2020-11-23 16:59:13 -0700

    completes user form

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const db = require('../models')
 const router = express.Router();
+
+// //add an entry to the table
+// app.post('/', (req, res) => {
+//     db.user.findByPk()
+//     res.send('this is the dashboard');
+// });
+
+// //show all entries in table
+// app.get('/', (req, res) => {
+//     db.user.findByPk()
+//     res.send('this is the dashboard');
+// });
+
+
+
+
+
+
+
+module.exports = router;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const isLoggedIn = require('../middleware/isLoggedIn');
-const db = require('../models')
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index')
-});
